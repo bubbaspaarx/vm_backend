56a7e2a626f219ec645537ba4859b00eceb03e41
Edward Sparks <edd_sparks@hotmail.com>
2019-01-07 15:12:09 +0000

    rack cors adjustment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://vm-cleo-frontend.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+
   end
 end
