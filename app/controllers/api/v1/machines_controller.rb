6fdd5e78cfbd2835aa9a2b1ffa2cf146cbf4dd30
Edward Sparks <edd_sparks@hotmail.com>
2019-01-09 13:22:57 +0000

    quick refactor of change generator. have put it in a module under lib/give_change.rb

--- app/controllers/api/v1/machines_controller.rb
+++ app/controllers/api/v1/machines_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::MachinesController < ApplicationController
   before_action :set_machine, only: [:show]
+  include GiveChange
 
   def show
     render json: @machine
@@ -48,39 +49,6 @@ class Api::V1::MachinesController < ApplicationController
   def return_change(total_due)
     @all_coins = {}
     @cash_avail = MachineCash.where('machine_id = ? AND quantity > ?', 1, 0)
-    def create_change(change_due)
-      # p "total due 1 #{change_due}"
-      if change_due == 0
-        @all_coins
-      else
-        cash_array = @cash_avail.sort_by{|coin| coin.quantity}.reverse!
-        counter = 0
-        cash_array.each do |coin_cash|
-          coin = coin_cash.money.value
-          # p "coin = #{coin}"
-          if change_due == 0 || counter == cash_array.length
-            return
-          elsif coin > change_due
-            counter += 1
-          elsif change_due % coin == 0 && change_due != 0
-            # puts "total due"
-            # p change_due
-            change_due -= coin
-            coin_cash.quantity -= 1
-            coin_cash.save
-            @all_coins[coin] ? @all_coins[coin] += 1 : @all_coins[coin] = 1
-            # puts "total due at end"
-            # p change_due
-            create_change(change_due) if change_due > 0
-            return
-          else
-            counter += 1
-          end
-        end
-        return @all_coins
-      end
-      return @all_coins
-    end
     create_change(total_due)
     return @all_coins
   end
